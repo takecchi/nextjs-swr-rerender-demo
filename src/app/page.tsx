937967c9f5ddeaa52e3031d338069768754ac1aa
takecchi <takecchi.kobayashi@gmail.com>
2023-09-13 07:03:33 +0900

    コンポーネント単位に分割

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,37 +11,68 @@ function formatToHHMMSS(date: Date | undefined): string {
 }
 
 export default function Home() {
-    const {data} = useDate();
-
     return (
         <main className="">
             <div className="flex justify-center gap-10"
                  style={{minHeight: '100px'}}>
                 <div className="border-solid border-2">
                     <p>1秒ごとに更新</p>
-                    <p>{formatToHHMMSS(data?.each1s)}</p>
+                    <Each1s/>
                 </div>
                 <div className="border-solid border-2">
                     <p>2秒ごとに更新</p>
-                    <p>{formatToHHMMSS(data?.each2s)}</p>
+                    <Each2s/>
                 </div>
                 <div className="border-solid border-2">
                     <p>3秒ごとに更新</p>
-                    <p>{formatToHHMMSS(data?.each3s)}</p>
+                    <Each3s/>
                 </div>
                 <div className="border-solid border-2">
                     <p>4秒ごとに更新</p>
-                    <p>{formatToHHMMSS(data?.each4s)}</p>
+                    <Each4s/>
                 </div>
                 <div className="border-solid border-2">
                     <p>5秒ごとに更新</p>
-                    <p>{formatToHHMMSS(data?.each5s)}</p>
+                    <Each5s/>
                 </div>
                 <div className="border-solid border-2">
                     <p>6秒ごとに更新</p>
-                    <p>{formatToHHMMSS(data?.each6s)}</p>
+                    <Each6s/>
                 </div>
             </div>
         </main>
     )
+}
+
+const Each1s = () => {
+    const {data} = useDate();
+    return <p>{formatToHHMMSS(data?.each1s)}</p>;
+}
+
+const Each2s = () => {
+    const {data} = useDate();
+    return <p>{formatToHHMMSS(data?.each2s)}</p>;
+}
+
+const Each3s = () => {
+    const {data} = useDate();
+    return <p>{formatToHHMMSS(data?.each3s)}</p>;
+}
+
+
+const Each4s = () => {
+    const {data} = useDate();
+    return <p>{formatToHHMMSS(data?.each4s)}</p>;
+}
+
+
+const Each5s = () => {
+    const {data} = useDate();
+    return <p>{formatToHHMMSS(data?.each5s)}</p>;
+}
+
+
+const Each6s = () => {
+    const {data} = useDate();
+    return <p>{formatToHHMMSS(data?.each6s)}</p>;
 }
